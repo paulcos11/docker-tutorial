5339bfab0c89ee25734ceb5e5efe06cd12eb5294
paulcos11 <paulcostello11@gmail.com>
2016-03-06 22:43:54 +0000

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Paul on 06/03/2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello again!");
+    }
+}
+
